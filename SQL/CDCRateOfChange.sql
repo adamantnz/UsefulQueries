e81189948e2b32f80e3912bfbfddcafd4ffd04a9
adamantnz <adamericgibson@gmail.com>
2015-12-18 12:04:13 +0000

    Formatting

--- SQL/CDCRateOfChange.sql
+++ SQL/CDCRateOfChange.sql
@@ -1,27 +1,26 @@
 DECLARE
 
-  @query nvarchar(max),
-  @cdcTable nvarchar(max) = '',
-  @cdcInstance nvarchar(max) = '',
-  @DB nvarchar(max) = ''
+   @query NVARCHAR(max)
+	,@cdcTable NVARCHAR(max) = ''
+	,@cdcInstance NVARCHAR(max) = ''
+	,@DB NVARCHAR(max) = ''
 
 BEGIN
-
-  SET @query =
-  'select column_name, sum(cnt) as change from (
-      select __$update_mask, count(*) cnt, sys.fn_cdc_is_bit_set ( c.column_id , __$update_mask ) isset, c.column_id, c.column_name
+	SET @query = 'SELECT column_name, SUM(cnt) as change FROM (
+      SELECT __$update_mask, COUNT(*) as cnt, sys.fn_cdc_is_bit_set
+        ( c.column_id , __$update_mask ) isset, c.column_id, c.column_name
     FROM '+@cdctable+'
-    CROSS APPLY (select column_id, column_name
+  CROSS APPLY (select column_id, column_name
   FROM '+@DB+'.cdc.change_tables t
-  INNER JOIN '+@DB+'.cdc.captured_columns c on t.Object_id = c.object_id
-  where capture_instance = '''+@cdcInstance+''') c
-  Group by __$update_mask, sys.fn_cdc_is_bit_set ( c.column_id , __$update_mask ), c.column_id, c.column_name
+    INNER JOIN '+@DB+'.cdc.captured_columns c on t.Object_id = c.object_id
+  WHERE capture_instance = '''+@cdcInstance+''') c
+  GROUP BY __$update_mask, sys.fn_cdc_is_bit_set ( c.column_id , __$update_mask ),
+    c.column_id, c.column_name
   having sys.fn_cdc_is_bit_set ( c.column_id , __$update_mask ) = 1
   ) a
   group by column_name
   order by 2 desc'
 
-PRINT (@query)
-EXEC (@query)
-
+	PRINT @query
+	EXEC sp_executesql @query
 END
